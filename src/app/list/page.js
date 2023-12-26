96e6ccced8a3e9081db046a8051fb5a6ff3c2583
송지훈 <wlgnsdl7368@gmail.com>
2023-12-26 15:55:42 +0900

    지훈,호석 프로젝트

---
+++ src/app/list/page.js
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+
+export default function List() {
+    const name = 'park'
+    return (
+        <>
+            <h2 className='title'>상품리스트입니다</h2>
+            <div>
+                <h4>상품1 $40</h4>
+            </div>
+            <div>
+                <h4>상품1 $40</h4>
+            </div>
+        </>
+    )
+}
