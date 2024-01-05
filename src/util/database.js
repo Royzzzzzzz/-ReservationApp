f5a52755b4dcdcdd04169a19484da69917cc3a04
송지훈 <wlgnsdl7368@gmail.com>
2024-01-05 14:32:40 +0900

    배포

--- src/util/database.js
+++ src/util/database.js
@@ -1,6 +1,6 @@
 import { MongoClient } from 'mongodb'
 
-const url = process.env.NEXT_PUBLIC_DATABASE_URL
+const url = process.env.DATABASE_URL
 const options = { useNewUrlParser: true }
 let connectDB
 
