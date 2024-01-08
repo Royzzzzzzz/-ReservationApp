0c34c4322dfaa664251b6d188765fa379f68fac4
송지훈 <wlgnsdl7368@gmail.com>
2024-01-08 16:20:47 +0900

    메뉴구성 변경

--- src/app/page.js
+++ src/app/page.js
@@ -1,67 +1,26 @@
-"use client";
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import '../styles/PageStyles.css';
-import lottie from 'lottie-web';
 
 import Button from '@mui/material/Button';
-import FaceIcon from '@mui/icons-material/Face';
-import CoffeeMakerIcon from '@mui/icons-material/CoffeeMaker';
-import RestaurantIcon from '@mui/icons-material/Restaurant';
-import HotelIcon from '@mui/icons-material/Hotel';
 
 export default function Home() {
-  const foodAnimationRef = useRef(null);
-  const coffeeAnimationRef = useRef(null);
-  const hairAnimationRef = useRef(null);
-  const bookAnimationRef = useRef(null);
-
-  useEffect(() => {
-    lottie.loadAnimation({
-      container: foodAnimationRef.current, // 애니메이션을 적용할 요소
-      renderer: 'svg',
-      loop: true,
-      autoplay: true,
-      path: '/svgs/foodAnimation.json' // 애니메이션 파일 경로
-    });
-    lottie.loadAnimation({
-      container: coffeeAnimationRef.current,
-      renderer: 'svg',
-      loop: true,
-      autoplay: true,
-      path: '/svgs/coffeeAnimation.json' // 커피 애니메이션 파일 경로
-    });
-    lottie.loadAnimation({
-      container: hairAnimationRef.current,
-      renderer: 'svg',
-      loop: true,
-      autoplay: true,
-      path: '/svgs/hairAnimation.json' // 미용 애니메이션 파일 경로
-    });
-    lottie.loadAnimation({
-      container: bookAnimationRef.current,
-      renderer: 'svg',
-      loop: true,
-      autoplay: true,
-      path: '/svgs/bookAnimation.json' // 미용 애니메이션 파일 경로
-    });
-  }, []);
   return (
     <div className="button-container">
       <Button color="primary" className="animated-button">
-        <div className="animation-container" ref={hairAnimationRef}></div>
-        <span className="button-text">미용</span>
+        <img src="/images/menu_001.png" alt="한식" />
+        <span className="button-text">한식</span>
       </Button>
       <Button color="primary" className="animated-button">
-        <div className="animation-container" ref={coffeeAnimationRef}></div>
-        <span className="button-text">카페</span>
+        <img src="/images/menu_002.png" alt="일식" />
+        <span className="button-text">일식</span>
       </Button>
       <Button color="primary" className="animated-button">
-        <div className="animation-container" ref={foodAnimationRef}></div>
-        <span className="button-text">음식</span>
+        <img src="/images/menu_003.png" alt="중식" />
+        <span className="button-text">중식</span>
       </Button>
       <Button color="primary" className="animated-button">
-        <div className="animation-container" ref={bookAnimationRef}></div>
-        <span className="button-text">숙박</span>
+        <img src="/images/menu_004.png" alt="디저트" />
+        <span className="button-text">디저트</span>
       </Button>
     </div>
   );
