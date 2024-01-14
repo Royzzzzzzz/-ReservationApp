19db13bc01d1c3b7e6439b59896bd65846159ddb
송지훈 <wlgnsdl7368@gmail.com>
2024-01-14 23:02:56 +0900

    1/14 커밋

---
+++ dist/next.config.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+/** @type {import('next').NextConfig} */
+var nextConfig = {
+  experimental: {
+    esmExternals: true
+  }
+};
+module.exports = nextConfig;
--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,34 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import '../styles/PageStyles.css';
 import Button from '@mui/material/Button';
+import SwiperCore, { Navigation, Scrollbar } from "swiper";
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css';
+
 
 // const AdCarousel = React.lazy(() => import('./_components/slide'));
 export default function Home() {
   return (
     <>
-      {/* <Suspense fallback={<div>Loading...</div>}>
-        <AdCarousel/>
-      </Suspense> */}
-    <div className="button-container">
-      <Button color="primary" className="animated-button">
-        <img src="/images/menu_001.png" alt="한식" />
-        <span className="button-text">한식</span>
-      </Button>
-      <Button color="primary" className="animated-button">
-        <img src="/images/menu_002.png" alt="일식" />
-        <span className="button-text">일식</span>
-      </Button>
-      <Button color="primary" className="animated-button">
-        <img src="/images/menu_003.png" alt="중식" />
-        <span className="button-text">중식</span>
-      </Button>
-      <Button color="primary" className="animated-button">
-        <img src="/images/menu_004.png" alt="디저트" />
-        <span className="button-text">디저트</span>
-      </Button>
-    </div>
+      <div className="button-container">
+        <Button color="primary" className="animated-button">
+          <img src="/images/menu_001.png" alt="한식" />
+          <span className="button-text">한식</span>
+        </Button>
+        <Button color="primary" className="animated-button">
+          <img src="/images/menu_002.png" alt="일식" />
+          <span className="button-text">일식</span>
+        </Button>
+        <Button color="primary" className="animated-button">
+          <img src="/images/menu_003.png" alt="중식" />
+          <span className="button-text">중식</span>
+        </Button>
+        <Button color="primary" className="animated-button">
+          <img src="/images/menu_004.png" alt="디저트" />
+          <span className="button-text">디저트</span>
+        </Button>
+      </div>
     </>
   );
 }
