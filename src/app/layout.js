5654fc70c97d64b74bd5713d775edc02ca820788
roy <wlgnsdl7368@gmail.com>
2024-01-16 16:38:00 +0900

    재배포

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,8 @@
-// "use client";
-import Link from "next/link";
+"use client";
 import "./globals.css";
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-import InputBase from "@mui/material/InputBase";
-import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
-import SearchIcon from "@mui/icons-material/Search";
-import { Nanum_Gothic } from "next/font/google";
 import ReduxProvider from "@/redux/provider";
-
-const nanum_Gothic = Nanum_Gothic({ weight: "400", subsets: ["latin"] });
+import Header from "./_components/header";
 
 export const metadata = {
   title: "포장의, 포장에 의한, 포장을 위한",
@@ -23,53 +12,9 @@ export const metadata = {
 export default async function RootLayout({ children }) {
   return (
     <html>
-      <body className={nanum_Gothic.className}>
-        <AppBar
-          sx={{
-            backgroundColor: "white",
-            maxWidth: "1024px",
-            margin: "auto",
-            boxShadow: "none",
-          }}
-          position="static"
-          className="header"
-        >
-          <Toolbar>
-            <IconButton
-              edge="start"
-              color="black"
-              aria-label="menu"
-              sx={{ mr: 1 }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography
-              variant="h5"
-              component="div"
-              sx={{ flexGrow: 1, color: "black" }}
-            >
-              <Link href="/list">db테스트</Link>
-            </Typography>
-            <Typography
-              variant="h5"
-              component="div"
-              sx={{ flexGrow: 1, color: "black" }}
-            >
-              <Link href="/">포장의 민족</Link>
-            </Typography>
-            <div>
-              <IconButton type="submit" aria-label="search">
-                <SearchIcon />
-              </IconButton>
-            </div>
-            <Button color="inherit">
-              <Link href="/login">
-                <Typography sx={{ color: "black" }}>로그인</Typography>
-              </Link>
-            </Button>
-          </Toolbar>
-        </AppBar>
+      <body>
         <Box sx={{ maxWidth: "1024px", margin: "auto" }}>
+          <Header />
           <ReduxProvider>{children}</ReduxProvider>
         </Box>
       </body>
